d8a3377d2fcce21be1cf8552277d47330021a496
Joshua Mühlfort <muehlfort@gonicus.de>
2022-10-17 14:16:36 +0200

    Change database example in debugging setup to postgres
    Signed-off-by: Joshua Mühlfort <muehlfort@gonicus.de>

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var db *gorm.DB
 
 func main() {
 	// Reading config
-	dbDsn := flag.String("db.dsn", "host=127.0.0.1 user=root dbname=defaultdb port=26257 sslmode=disable", "DB dsn")
+	dbDsn := flag.String("postgres-dsn", "host=127.0.0.1 user=postgres dbname=postgres port=5432 sslmode=disable", "DB dsn")
 	componentsFile := flag.String("components-file", "./components.yaml", "YAML file containing components")
 	addr := flag.String("addr", ":3000", "Address to listen on")
 	var corsOrigins string
