c6785f8c43e6ca518121beafb47ee1d93e7a2b54
Joshua Mühlfort <muehlfort@gonicus.de>
2022-10-20 09:25:53 +0200

    Make provisioning of components more generic
    Signed-off-by: Joshua Mühlfort <muehlfort@gonicus.de>

--- components.go
+++ components.go
@@ -1,43 +1,13 @@
 package main
 
 import (
-	"os"
-
 	"github.com/labstack/echo/v4"
-	"gopkg.in/yaml.v3"
 	"gorm.io/gorm"
 )
 
 type Component struct {
 	Slug      string      `gorm:"primaryKey" json:"slug"`
 	Incidents []*Incident `gorm:"many2many:incident_component;" json:"incidents,omitempty"`
-}
-
-func loadComponents(filename string) error {
-	file, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	configComponents := []Component{}
-	err = yaml.NewDecoder(file).Decode(&configComponents)
-	if err != nil {
-		return err
-	}
-	for _, configComponent := range configComponents {
-		err = db.Take(&configComponent).Error
-		switch err {
-		case gorm.ErrRecordNotFound:
-			saveErr := db.Save(&configComponent).Error
-			if saveErr != nil {
-				return saveErr
-			}
-		case nil:
-		default:
-			return err
-		}
-	}
-	return nil
 }
 
 func componentList(c echo.Context) error {
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 )
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
@@ -32,4 +33,6 @@ require (
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	gorm.io/datatypes v1.0.7 // indirect
+	gorm.io/driver/mysql v1.4.3 // indirect
 )
