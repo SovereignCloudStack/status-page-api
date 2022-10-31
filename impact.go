ae244a42a8e7e5a685638489dc2dd801a939b464
Joshua Mühlfort <muehlfort@gonicus.de>
2022-10-31 09:46:30 +0100

    Add provisioning check to impactTypes
    Signed-off-by: Joshua Mühlfort <muehlfort@gonicus.de>

--- impact.go
+++ impact.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
 type ImpactType struct {
-	Slug string `gorm:"primaryKey" json:"slug"`
+	Slug        string `gorm:"primaryKey" json:"slug"`
+	provisioned bool   `json:""`
+}
+
+func (t *ImpactType) BeforeCreate(tx *gorm.DB) error {
+	if tx.Take(t).Error == gorm.ErrRecordNotFound && !t.provisioned {
+		return fmt.Errorf("attempted to create non-provisioned phase %v", t.Slug)
+	}
+	return nil
+}
+
+func (t *ImpactType) BeforeUpdate(tx *gorm.DB) error {
+	if tx.Take(t).Error == gorm.ErrRecordNotFound && !t.provisioned {
+		return fmt.Errorf("attempted to update non-provisioned phase %v", t.Slug)
+	}
+	return nil
 }
