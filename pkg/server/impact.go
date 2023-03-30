9b5cf3cee2c1d80a29eb20c8b047053801aa9371
Joshua Irmer <irmer@gonicus.de>
2023-03-30 12:10:48 +0200

    WIP: api server interface implementation
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

---
+++ pkg/server/impact.go
@@ -0,0 +1,7 @@
+package server
+
+import "github.com/labstack/echo/v4"
+
+func (s *ServerImplementation) GetImpacttypes(ctx echo.Context) error {
+	return nil
+}
---
+++ pkg/server/phase.go
@@ -0,0 +1,7 @@
+package server
+
+import "github.com/labstack/echo/v4"
+
+func (s *ServerImplementation) GetPhases(ctx echo.Context) error {
+	return nil
+}
