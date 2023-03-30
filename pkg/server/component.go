a7b0a38be9f098e5d76c8073ecd44b179084c9e9
Joshua Irmer <irmer@gonicus.de>
2023-03-30 12:10:48 +0200

    WIP: api server interface implementation
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

---
+++ pkg/server/component.go
@@ -0,0 +1,10 @@
+package server
+
+import "github.com/labstack/echo/v4"
+
+func (s *ServerImplementation) GetComponent(ctx echo.Context, componentId string) error {
+	return nil
+}
+func (s *ServerImplementation) GetComponents(ctx echo.Context) error {
+	return nil
+}
---
+++ pkg/server/incident.go
@@ -0,0 +1,13 @@
+package server
+
+import (
+	"github.com/SovereignCloudStack/status-page-openapi/pkg/api"
+	"github.com/labstack/echo/v4"
+)
+
+func (s *ServerImplementation) GetIncident(ctx echo.Context, incidentId string) error {
+	return nil
+}
+func (s *ServerImplementation) GetIncidents(ctx echo.Context, params api.GetIncidentsParams) error {
+	return nil
+}
