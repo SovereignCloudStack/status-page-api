17721885b53f63a7fe61623cfff1f6d7461b9549
Joshua Irmer <irmer@gonicus.de>
2023-04-04 16:22:26 +0200

    rework incident to correctly relate data
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

--- pkg/db/incident.go
+++ pkg/db/incident.go
@@ -1,21 +1,41 @@
 package db
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Incident struct {
-	Id          Id               `gorm:"primaryKey" json:"id"`
-	Affects     []Component      `gorm:"many2many:component_incidents" json:"affects"`
-	BeganAt     *time.Time       `json:"beganAt,omitempty"`
-	Description string           `json:"description"`
-	EndedAt     *time.Time       `json:"endedAt"`
-	ImpactType  ImpactType       `gorm:"many2many:incident_impact_types;references:slug" json:"impactType"`
-	Phase       Phase            `gorm:"foreignKey:slug" json:"phase"`
-	Title       string           `json:"title"`
-	Updates     []IncidentUpdate `gorm:"foreignKey:Id" json:"updates"`
+	Id             Id               `gorm:"primaryKey" json:"id"`
+	Affects        []Component      `gorm:"many2many:component_incidents" json:"affects"`
+	BeganAt        *time.Time       `json:"beganAt,omitempty"`
+	Description    string           `json:"description"`
+	EndedAt        *time.Time       `json:"endedAt"`
+	ImpactTypeSlug string           `json:"-"`
+	ImpactType     ImpactType       `gorm:"foreignKey:ImpactTypeSlug" json:"impactType"`
+	PhaseSlug      string           `json:"-"`
+	Phase          Phase            `gorm:"foreignKey:PhaseSlug" json:"phase"`
+	Title          string           `json:"title"`
+	Updates        []IncidentUpdate `json:"updates"`
 }
 
 type IncidentUpdate struct {
-	Id        Id        `gorm:"primaryKey"`
-	CreatedAt time.Time `json:"createdAt"`
-	Text      string    `json:"text"`
+	Id         Id        `gorm:"primaryKey"`
+	CreatedAt  time.Time `json:"createdAt"`
+	Text       string    `json:"text"`
+	IncidentId Id        `json:"-"`
+}
+
+func (i *Incident) BeforeCreate(tx *gorm.DB) error {
+	i.Id = Id(uuid.NewString())
+
+	return nil
+}
+
+func (iu *IncidentUpdate) BeforeCreate(tx *gorm.DB) error {
+	iu.Id = Id(uuid.NewString())
+
+	return nil
 }
