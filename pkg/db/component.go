93ba51e10101b2ed8efd865be29f931cede79298
Joshua Irmer <irmer@gonicus.de>
2023-04-04 16:20:41 +0200

    add uuid generation for id field
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

--- pkg/db/component.go
+++ pkg/db/component.go
@@ -1,8 +1,19 @@
 package db
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type Component struct {
 	Id          Id         `gorm:"primaryKey" json:"id"`
 	AffectedBy  []Incident `gorm:"many2many:component_incidents" json:"affectedBy"`
 	DisplayName string     `json:"displayName"`
-	Labels      []Label    `gorm:"many2many:component_labels" json:"labels"`
+	Labels      Labels     `gorm:"many2many:component_labels" json:"labels"`
+}
+
+func (c *Component) BeforeCreate(tx *gorm.DB) error {
+	c.Id = Id(uuid.NewString())
+
+	return nil
 }
