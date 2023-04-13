da5ea2b650acca3cf08d40e941b926428d5c044b
Joshua Irmer <irmer@gonicus.de>
2023-04-13 16:13:30 +0200

    serve components
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

--- pkg/server/component.go
+++ pkg/server/component.go
@@ -1,11 +1,49 @@
 package server
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
 
-func (s *Implementation) GetComponent(ctx echo.Context, componentId string) error {
-	return nil
+	DbDef "github.com/SovereignCloudStack/status-page-api/pkg/db"
+	"github.com/SovereignCloudStack/status-page-openapi/pkg/api"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm/clause"
+)
+
+func (i *Implementation) GetComponent(ctx echo.Context, componentId string) error {
+	var component DbDef.Component
+
+	res := i.dbCon.Preload(clause.Associations).Where("id = ?", componentId).First(&component)
+
+	err := res.Error
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error)
+	}
+
+	return ctx.JSON(http.StatusOK, api.Component{
+		AffectedBy:  component.GetAffectedByIDs(),
+		DisplayName: component.DisplayName,
+		Id:          string(component.ID),
+		Labels:      component.GetLabelMap(),
+	})
 }
 
-func (s *Implementation) GetComponents(ctx echo.Context) error {
-	return nil
+func (i *Implementation) GetComponents(ctx echo.Context) error {
+	var components []DbDef.Component
+
+	res := i.dbCon.Preload(clause.Associations).Find(&components)
+
+	err := res.Error
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	componentList := make([]api.Component, len(components))
+	for componentIndex, component := range components {
+		componentList[componentIndex].AffectedBy = component.GetAffectedByIDs()
+		componentList[componentIndex].DisplayName = component.DisplayName
+		componentList[componentIndex].Id = string(component.ID)
+		componentList[componentIndex].Labels = component.GetLabelMap()
+	}
+
+	return ctx.JSON(http.StatusOK, componentList)
 }
