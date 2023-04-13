a0ff4bcbd8dcd0544908e2af06a64a5c16e57a39
Joshua Irmer <irmer@gonicus.de>
2023-04-13 15:29:10 +0200

    serve phase list
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

--- pkg/server/phase.go
+++ pkg/server/phase.go
@@ -1,7 +1,27 @@
 package server
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
 
-func (s *Implementation) GetPhases(ctx echo.Context) error {
-	return nil
+	DbDef "github.com/SovereignCloudStack/status-page-api/pkg/db"
+	"github.com/SovereignCloudStack/status-page-openapi/pkg/api"
+	"github.com/labstack/echo/v4"
+)
+
+func (i *Implementation) GetPhases(ctx echo.Context) error {
+	var phases []DbDef.Phase
+
+	res := i.dbCon.Find(&phases)
+
+	err := res.Error
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	phaseList := make([]api.IncidentPhase, len(phases))
+	for _, phase := range phases {
+		phaseList[phase.Order] = phase.Slug
+	}
+
+	return ctx.JSON(http.StatusOK, phaseList)
 }
