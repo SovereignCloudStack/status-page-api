5b502b284c3c37ef9b52722571e9f505df822210
Joshua Irmer <irmer@gonicus.de>
2023-04-13 15:28:43 +0200

    serve impact type list
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

--- pkg/server/impact.go
+++ pkg/server/impact.go
@@ -1,7 +1,27 @@
 package server
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
 
-func (s *Implementation) GetImpacttypes(ctx echo.Context) error {
-	return nil
+	DbDef "github.com/SovereignCloudStack/status-page-api/pkg/db"
+	"github.com/SovereignCloudStack/status-page-openapi/pkg/api"
+	"github.com/labstack/echo/v4"
+)
+
+func (i *Implementation) GetImpacttypes(ctx echo.Context) error {
+	var impactTypes []DbDef.ImpactType
+
+	res := i.dbCon.Find(&impactTypes)
+
+	err := res.Error
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	impactTypeList := make([]api.IncidentImpactType, len(impactTypes))
+	for impactTypeIndex, impactType := range impactTypes {
+		impactTypeList[impactTypeIndex] = impactType.Slug
+	}
+
+	return ctx.JSON(http.StatusOK, impactTypeList)
 }
