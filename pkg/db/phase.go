49f4fe86c870d797aa39528dac534d8b584599c7
Joshua Irmer <irmer@gonicus.de>
2023-06-07 16:19:51 +0200

    update phase to new structure
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

--- pkg/db/phase.go
+++ pkg/db/phase.go
@@ -1,7 +1,10 @@
 package db
 
+import "github.com/SovereignCloudStack/status-page-openapi/pkg/api"
+
 // Phase represents a state of an incident on a movin scale to resolution of the incident.
 type Phase struct {
-	Slug  string `gorm:"primaryKey" json:"slug"`
-	Order uint   `gorm:"unique"`
+	api.Phase
+	Generation api.Incremental `gorm:"primaryKey"`
+	Order      api.Incremental `gorm:"primaryKey"`
 }
