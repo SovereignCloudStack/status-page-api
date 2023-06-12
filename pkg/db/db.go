9740ca0d36fe1476ed030994a8f7dc44a481bc0c
Joshua Irmer <irmer@gonicus.de>
2023-06-12 16:55:22 +0200

    update provisioning
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -5,30 +5,27 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/google/uuid"
 	"gopkg.in/yaml.v3"
 	"gorm.io/gorm"
 )
 
 // ID is the generally used identifier type. String type for UUIDs.
-type ID string
+type ID = uuid.UUID
+
+type Model struct {
+	ID *ID `gorm:"primaryKey;type:uuid;"`
+}
+
+func (m *Model) BeforeCreate(_ *gorm.DB) error {
+	id := uuid.New()
+	m.ID = &id
+
+	return nil
+}
 
 // Provision initializes the database with the contents of the provision file.
-func Provision(filename string, dbCon *gorm.DB) error {
-	var donePhase Phase
-	res := dbCon.Where("slug = ?", "done").First(&donePhase)
-
-	err := res.Error
-	if err != nil {
-		if !errors.Is(err, gorm.ErrRecordNotFound) {
-			return fmt.Errorf("error getting `done` phase: %w", err)
-		}
-	}
-
-	if donePhase.Order != 0 {
-		// db has been provisioned before
-		return nil
-	}
-
+func Provision(filename string, dbCon *gorm.DB) error { //nolint:funlen,cyclop
 	type ProvisionedResources struct {
 		Components  []*Component  `yaml:"components"`
 		ImpactTypes []*ImpactType `yaml:"impactTypes"`
@@ -41,71 +38,61 @@ func Provision(filename string, dbCon *gorm.DB) error {
 	}
 	defer file.Close()
 
-	resources := ProvisionedResources{}
+	resources := ProvisionedResources{} //nolint:exhaustruct
 
 	err = yaml.NewDecoder(file).Decode(&resources)
 	if err != nil {
 		return fmt.Errorf("error decoding provisioning file `%s`: %w", filename, err)
 	}
 
+	initialPhaseGeneration := 1
+
+	// check if already provisioned
+	var lastPhase Phase
+
+	res := dbCon.
+		Where("generation = ? AND name = ?", initialPhaseGeneration, resources.Phases[len(resources.Phases)-1].Name).
+		First(&lastPhase)
+
+	err = res.Error
+	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return fmt.Errorf("error getting last phase: %w", err)
+		}
+	}
+
+	if lastPhase.Order != nil && *lastPhase.Order == len(resources.Phases)-1 {
+		// db has been provisioned before
+		return nil
+	}
+
 	for _, component := range resources.Components {
 		err = dbCon.Save(component).Error
 		if err != nil {
-			return fmt.Errorf("error saving component `%s`: %w", component.DisplayName, err)
+			return fmt.Errorf("error saving component `%s`: %w", *component.DisplayName, err)
 		}
 	}
 
 	for _, impactType := range resources.ImpactTypes {
 		err = dbCon.Save(impactType).Error
 		if err != nil {
-			return fmt.Errorf("error saving impact type `%s`: %w", impactType.Slug, err)
+			return fmt.Errorf("error saving impact type `%s`: %w", *impactType.DisplayName, err)
 		}
 	}
 
-	var phaseOrder uint
-	for phaseIndex := range resources.Phases {
-		resources.Phases[phaseIndex].Order = phaseOrder
+	var phaseOrder int
 
-		err = dbCon.Save(&resources.Phases[phaseIndex]).Error
+	for phaseIndex := range resources.Phases {
+		phase := resources.Phases[phaseIndex]
+		phase.Order = &phaseOrder
+		phase.Generation = &initialPhaseGeneration
+
+		err = dbCon.Save(&phase).Error
 		if err != nil {
-			return fmt.Errorf("error saving phase `%s`: %w", resources.Phases[phaseIndex].Slug, err)
+			return fmt.Errorf("error saving phase `%s`: %w", *phase.Name, err)
 		}
 
 		phaseOrder++
-	}
-
-	// always add done phase as last
-	err = dbCon.Save(&Phase{
-		Slug:  "done",
-		Order: phaseOrder,
-	}).Error
-	if err != nil {
-		return fmt.Errorf("error saving phase `done`: %w", err)
-	}
-
-	return nil
-}
-
-// UnmarshalYAML implments the unmarshaling for labels, as they are being read as map and converted to [Label] object.
-func (l *Labels) UnmarshalYAML(value *yaml.Node) error {
-	if value.Kind != yaml.MappingNode {
-		return fmt.Errorf("error unmarshaling: %w", ErrLabelFormat)
-	}
-
-	const contentFieldSize = 2
-
-	*l = make(Labels, len(value.Content)/contentFieldSize)
-
-	for contentIndex := 0; contentIndex < len(value.Content); contentIndex += contentFieldSize {
-		res := &(*l)[contentIndex/contentFieldSize]
-
-		if err := value.Content[contentIndex].Decode(&res.Name); err != nil {
-			return fmt.Errorf("error decoding label name: %w", err)
-		}
-
-		if err := value.Content[contentIndex+1].Decode(&res.Value); err != nil {
-			return fmt.Errorf("error decoding label value: %w", err)
-		}
 	}
 
 	return nil
