21be25cae336dc4ea2adb2cab029f001eab08704
Joshua Irmer <irmer@gonicus.de>
2023-06-12 12:14:29 +0200

    fix typo
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

--- pkg/db/phase.go
+++ pkg/db/phase.go
@@ -2,7 +2,7 @@ package db
 
 import "github.com/SovereignCloudStack/status-page-openapi/pkg/api"
 
-// Phase represents a state of an incident on a movin scale to resolution of the incident.
+// Phase represents a state of an incident on a moving scale to resolution of the incident.
 type Phase struct {
 	api.Phase
 	Generation api.Incremental `gorm:"primaryKey"`
