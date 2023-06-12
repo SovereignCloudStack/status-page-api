ed51e0e54789d45dbc51411985008aa40d9c29c5
Joshua Irmer <irmer@gonicus.de>
2023-06-12 12:37:44 +0200

    convert to new API structure
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

--- pkg/db/impact.go
+++ pkg/db/impact.go
@@ -4,13 +4,16 @@ import "github.com/SovereignCloudStack/status-page-openapi/pkg/api"
 
 // ImpactType represents the type of impact.
 type ImpactType struct {
-	Model          `gorm:"embedded"`
 	api.ImpactType `gorm:"embedded"`
+	Model          `gorm:"embedded"`
 }
 
 type Impact struct {
-	IncidentID   ID         `gorm:"primaryKey"`
-	ComponentID  ID         `gorm:"primaryKey"`
-	ImpactTypeID ID         `gorm:"primaryKey"`
-	impactType   ImpactType `gorm:"foreignKey:ImpactTypeID"`
+	Incident   *Incident   `gorm:"foreignKey:IncidentID"`
+	Component  *Component  `gorm:"foreignKey:ComponentID"`
+	ImpactType *ImpactType `gorm:"foreignKey:ImpactTypeID"`
+
+	IncidentID   ID `gorm:"primaryKey"`
+	ComponentID  ID `gorm:"primaryKey"`
+	ImpactTypeID ID `gorm:"primaryKey"`
 }
--- pkg/db/incident.go
+++ pkg/db/incident.go
@@ -1,48 +1,26 @@
 package db
 
 import (
-	"time"
-
 	"github.com/SovereignCloudStack/status-page-openapi/pkg/api"
-	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 // Incident represents an incident happening to one or more [Component].
 type Incident struct {
-	ID             ID               `gorm:"primaryKey" json:"id"`
-	Affects        []Component      `gorm:"many2many:component_incidents" json:"affects"`
-	BeganAt        *time.Time       `json:"beganAt,omitempty"`
-	Description    string           `json:"description"`
-	EndedAt        *time.Time       `json:"endedAt"`
-	ImpactTypeSlug string           `json:"-"`
-	ImpactType     ImpactType       `gorm:"foreignKey:ImpactTypeSlug" json:"impactType"`
-	PhaseSlug      string           `json:"-"`
-	Phase          Phase            `gorm:"foreignKey:PhaseSlug" json:"phase"`
-	Title          string           `json:"title"`
-	Updates        []IncidentUpdate `gorm:"foreignKey:IncidentID" json:"updates"`
+	Affects         []*Impact `gorm:"foreignKey:IncidentID"`
+	BeganAt         *api.Date
+	Description     *api.Description
+	DisplayName     *api.DisplayName
+	EndedAt         *api.Date
+	PhaseGeneration *api.Incremental
+	PhaseOrder      *api.Incremental
+	Phase           *Phase            `gorm:"foreignKey:PhaseGeneration,PhaseOrder;References:Generation,Order"`
+	Updates         []*IncidentUpdate `gorm:"foreignKey:IncidentID"`
+	Model           `gorm:"embedded"`
 }
 
 // IncidentUpdate describes a action that changes the incident.
 type IncidentUpdate struct {
-	Model              `gorm:"embedded"`
 	api.IncidentUpdate `gorm:"embedded"`
-}
-
-// BeforeCreate implements the behavior before a database insertion. This adds an UUID as ID.
-func (i *Incident) BeforeCreate(_ *gorm.DB) error {
-	i.ID = uuid.New()
-
-	return nil
-}
-
-// GetAffectsIds is a helper function to convert the affected components to a list of [Component.ID]s.
-func (i *Incident) GetAffectsIds() []string {
-	componentIds := make([]string, len(i.Affects))
-
-	for componentIndex, component := range i.Affects {
-		componentIds[componentIndex] = component.ID.String()
-	}
-
-	return componentIds
+	Order              api.Incremental `gorm:"primaryKey"`
+	IncidentID         ID              `gorm:"primaryKey"`
 }
