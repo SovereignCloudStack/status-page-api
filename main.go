af4befe06b4663d9fc97782ec742ce0117c7270e
Joshua Irmer <irmer@gonicus.de>
2023-06-15 15:18:00 +0200

    use new api structure and zerolog
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

--- main.go
+++ main.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"flag"
+	"os"
 	"strings"
+	"time"
 
+	"github.com/SovereignCloudStack/status-page-api/internal/app/logger"
 	"github.com/SovereignCloudStack/status-page-api/internal/app/swagger"
 	DbDef "github.com/SovereignCloudStack/status-page-api/pkg/db"
 	"github.com/SovereignCloudStack/status-page-api/pkg/server"
 	"github.com/SovereignCloudStack/status-page-openapi/pkg/api"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func main() {
+func main() { //nolint:funlen
+	// setup logging
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{ //nolint:exhaustruct
+		Out:        os.Stderr,
+		TimeFormat: time.RFC3339,
+	})
+
+	echoLogger := log.With().Str("component", "echo").Logger()
+	gormLogger := log.With().Str("component", "gorm").Logger()
+	handlerLogger := log.With().Str("component", "handler").Logger()
+
 	// Reading config
 	dbDsn := flag.String(
 		"postgres-dsn",
@@ -41,10 +58,13 @@ func main() {
 
 	// HTTP setup
 	echoServer := echo.New()
-	echoServer.Use(middleware.Logger())
+	echoServer.HideBanner = true
+	echoServer.HidePort = true
+
+	echoServer.Use(logger.NewEchoLoggerConfig(&echoLogger))
 	echoServer.Use(middleware.Recover())
 	echoServer.Use(middleware.RemoveTrailingSlash())
-	echoServer.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	echoServer.Use(middleware.CORSWithConfig(middleware.CORSConfig{ //nolint:exhaustruct
 		AllowOrigins: strings.Split(*corsOrigins, ","),
 	}))
 
@@ -52,32 +72,35 @@ func main() {
 	echoServer.GET("/swagger", swagger.ServeSwagger)
 
 	// DB setup
-	dbCon, err := gorm.Open(postgres.Open(*dbDsn), &gorm.Config{})
+	dbCon, err := gorm.Open(postgres.Open(*dbDsn), &gorm.Config{ //nolint:exhaustruct
+		Logger: logger.NewGormLogger(&gormLogger),
+	})
 	if err != nil {
-		echoServer.Logger.Fatal(err)
+		log.Fatal().Err(err).Msg("error opening database connection")
 	}
 
 	err = dbCon.AutoMigrate(
-		&DbDef.Label{},
-		&DbDef.ImpactType{},
-		&DbDef.Phase{},
-		&DbDef.Incident{},
-		&DbDef.IncidentUpdate{},
-		&DbDef.Component{},
+		&DbDef.Component{},      //nolint:exhaustruct
+		&DbDef.Phase{},          //nolint:exhaustruct
+		&DbDef.IncidentUpdate{}, //nolint:exhaustruct
+		&DbDef.Incident{},       //nolint:exhaustruct
+		&DbDef.ImpactType{},     //nolint:exhaustruct
+		&DbDef.Impact{},         //nolint:exhaustruct
 	)
 	if err != nil {
-		echoServer.Logger.Fatal(err)
+		log.Fatal().Err(err).Msg("error migrating structures")
 	}
 
 	// Initialize "static" DB contents
 	err = DbDef.Provision(*provisioningFile, dbCon)
 	if err != nil {
-		echoServer.Logger.Fatal(err)
+		log.Fatal().Err(err).Msg("error provisioning data")
 	}
 
 	// register api
-	api.RegisterHandlers(echoServer, server.New(dbCon))
+	api.RegisterHandlers(echoServer, server.New(dbCon, &handlerLogger))
 
 	// Starting server
-	echoServer.Logger.Fatal(echoServer.Start(*addr))
+	log.Info().Str("address", *addr).Msg("server start listening")
+	log.Fatal().Err(echoServer.Start(*addr)).Msg("error running server")
 }
