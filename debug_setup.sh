c017fdbbfa90a80ee85bb66f175acfe88f55fbae
Joshua Irmer <irmer@gonicus.de>
2023-08-01 14:30:24 +0200

    remove old dev files
    Signed-off-by: Joshua Irmer <irmer@gonicus.de>

--- debug_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-podman network exists statuspage || podman network create statuspage
-
-# Start up Postgres compatible database
-podman container exists statuspage_database && podman container start statuspage_database || \
-  podman run --name statuspage_database --network statuspage -p 5432:5432 -e POSTGRES_PASSWORD=debug -d postgres
